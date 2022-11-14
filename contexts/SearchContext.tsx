2532775d99c4f063425ebfaaf7b7bf68a918fa71
Jinho <yoonjinho0318@gmail.com>
2022-11-14 17:12:39 -0500

    refactor: explicit nullity for SearchContext

--- contexts/SearchContext.tsx
+++ contexts/SearchContext.tsx
@@ -3,5 +3,5 @@ import { createContext, Dispatch, SetStateAction } from 'react'
 const SearchContext = createContext<{
   query: string
   setQuery: Dispatch<SetStateAction<string>>
-}>(null)
+} | null>(null)
 export default SearchContext
