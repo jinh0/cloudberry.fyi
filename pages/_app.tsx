be6a062de541f763444f696bb563bb16d0f4f796
Jinho <yoonjinho0318@gmail.com>
2022-11-16 17:43:36 -0500

    test: google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,7 @@ import { auth, db } from '@utils/firebase'
 import { doc } from 'firebase/firestore'
 import UserContext from '@contexts/UserContext'
 import { useDocument } from 'react-firebase-hooks/firestore'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
   const [loggedIn] = useAuthState(auth)
@@ -17,6 +18,19 @@ export default function App({ Component, pageProps }: AppProps) {
     <UserContext.Provider value={{ user, loading, error }}>
       <Head>
         <link rel='icon' href='/favicon.ico' />
+        <Script
+          strategy='lazyOnload'
+          src='https://www.googletagmanager.com/gtag/js?id=G-4HB680742L'
+        ></Script>
+        <Script strategy='lazyOnload' id='analytics'>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-4HB680742L');
+          `}
+        </Script>
       </Head>
       <Component {...pageProps} />
     </UserContext.Provider>
