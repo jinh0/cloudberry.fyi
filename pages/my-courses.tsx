bf7f8749b81e385c9d2fe9cd581470775eea208b
Jinho <yoonjinho0318@gmail.com>
2022-11-18 00:02:24 -0500

    fix: missing import in /my-courses

--- pages/my-courses.tsx
+++ pages/my-courses.tsx
@@ -10,7 +10,6 @@ import { auth } from '@utils/firebase'
 import { useRouter } from 'next/router'
 import { useContext } from 'react'
 import { useAuthState } from 'react-firebase-hooks/auth'
-import { find } from 'utils/courses'
 
 const MyCourses = () => {
   const router = useRouter()
@@ -33,8 +32,6 @@ const MyCourses = () => {
   return (
     <Main>
       <Title>Saved</Title>
-
-      <div>{saved.map((code) => find(code).name)}</div>
     </Main>
   )
 }
