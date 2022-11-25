c7d399165e319fdbdf8169d0c5a89360b2a956aa
Mark Horner <mark.horner@mail.mcgill.ca>
2022-11-25 14:54:05 -0500

    fix: day conversion from number

--- components/course/Block.tsx
+++ components/course/Block.tsx
@@ -73,13 +73,13 @@ const minToStr = (time: number) => {
 }
 
 const NUM_TO_DAY = {
-  1: 'Monday',
-  2: 'Tuesday',
-  3: 'Wednesday',
-  4: 'Thursday',
-  5: 'Friday',
-  6: 'Saturday',
-  7: 'Sunday',
+  0: 'Monday',
+  1: 'Tuesday',
+  2: 'Wednesday',
+  3: 'Thursday',
+  4: 'Friday',
+  5: 'Saturday',
+  6: 'Sunday',
 }
 
 export default Block
