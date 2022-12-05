c3dae0a06965ec2089c11c003f2e6adf2f0e84cb
lil1an <lilan0213@yahoo.com>
2022-12-04 23:06:50 -0500

    feat(button): add tooltip to copy link

--- components/course/ShareButton.tsx
+++ components/course/ShareButton.tsx
@@ -1,6 +1,8 @@
 import { LinkIcon } from '@heroicons/react/24/outline'
 import { CheckIcon } from '@heroicons/react/24/outline'
 import { useEffect, useState } from 'react'
+import { Tooltip, Button } from '@mantine/core'
+import { contents } from 'cheerio/lib/api/traversing'
 
 /**
  * Copy Link Button
@@ -18,23 +20,50 @@ const ShareButton = () => {
 
   return (
     <div className='flex flex-row items-center'>
-      <button
-        className='border-none rounded px-2.5 py-1 flex flex-row items-center gap-2'
-        onClick={() => { navigator.clipboard.writeText(window.location.href);  setcopied(true) }}
+      <Tooltip
+        label={copied ? 'Copied!' : 'Copy link'}
+        color='black'
+        withArrow
+        arrowPosition='center'
       >
-        <div className=''>
-          {copied ? (
-            <CheckIcon className='w-5 h-5 text-green-500' />
-          ) : (
-            <LinkIcon className='w-5 h-5 text-black' />
-          )}
-        </div>
-      </button>
-      <p className='text-sm font-medium text-gray-500'>
-        {copied ? 'Copied' : ''}
-      </p>
+        <Button
+          variant='outline'
+          className='border-none rounded px-2.5 py-1 flex flex-row items-center gap-2'
+          onClick={() => {
+            navigator.clipboard.writeText(window.location.href)
+            setcopied(true)
+          }}
+        >
+          <div className=''>
+            {copied ? (
+              <CheckIcon className='w-5 h-5 text-green-500' />
+            ) : (
+              <LinkIcon className='w-5 h-5 text-black' />
+            )}
+          </div>
+        </Button>
+      </Tooltip>
     </div>
   )
+
+  //   <div className='flex flex-row items-center'>
+  //     <button
+  //       className='border-none rounded px-2.5 py-1 flex flex-row items-center gap-2'
+  //       onClick={() => { navigator.clipboard.writeText(window.location.href);  setcopied(true) }}
+  //     >
+  //       <div className=''>
+  //         {copied ? (
+  //           <CheckIcon className='w-5 h-5 text-green-500' />
+  //         ) : (
+  //           <LinkIcon className='w-5 h-5 text-black' />
+  //         )}
+  //       </div>
+  //     </button>
+  //     <p className='text-sm font-medium text-gray-500'>
+  //       {copied ? 'Copied' : ''}
+  //     </p>
+  //   </div>
+  // )
 }
 
 export default ShareButton
