25b9223ecf61d48e750c1eaa90c968a09f7e8674
Jinho Yoon <yoonjinho0318@gmail.com>
2022-12-06 23:07:32 -0500

    feat: display user info with horizontal lists

--- components/user/UserInfo.tsx
+++ components/user/UserInfo.tsx
@@ -1,22 +1,43 @@
 import UserContext from '@contexts/UserContext'
+import useCourse from '@hooks/useCourse'
 import { UserType } from '@typing'
 import { useContext } from 'react'
+
+const CourseCard = ({ code }: { code: string }) => {
+  const format = (code: string) => code?.replace('-', ' ').toUpperCase()
+
+  const { data, isLoading } = useCourse(code)
+
+  if (isLoading) {
+    return (
+      <div className='border rounded-lg p-2 mb-4'>
+        <div className='w-full bg-gray-100 text-transparent select-none rounded-full animate-pulse'>
+          XXX
+        </div>
+      </div>
+    )
+  }
+
+  const course = data.result
+
+  return (
+    <div className='w-60 border rounded-lg p-2 px-4 flex-shrink-0 snap-start'>
+      <p className='font-semibold'>{format(course.code)}</p>
+      <p className=''>{course.name}</p>
+    </div>
+  )
+}
 
 const UserInfo = () => {
   const { user, loading, error } = useContext(UserContext)
 
-  if (loading) {
-    return <div>Loading...</div>
-  }
-
-  if (error || !user) {
-    return <div>Something went wrong.</div>
-  }
+  if (loading) return <div>Loading...</div>
+  if (error || !user) return <div>Something went wrong.</div>
 
   const { name, email, saved, completed, current } = user.data() as UserType
 
   return (
-    <div>
+    <div className='flex flex-col'>
       <p>
         <span className='font-bold'>Name:</span> {name}
       </p>
@@ -24,19 +45,29 @@ const UserInfo = () => {
         <span className='font-bold'>Email:</span> {email}
       </p>
 
-      <p>
-        <span className='font-bold'>Saved:</span> {saved && saved.join(', ')}
-      </p>
+      <div className='mt-8'>
+        <p className='text-2xl mb-4'>Saved Courses</p>
 
-      <p>
-        <span className='font-bold'>Completed:</span>{' '}
-        {completed && completed.join(', ')}
-      </p>
+        <div className='flex flex-row gap-x-4 overflow-x-auto snap-x snap-mandatory'>
+          {saved && saved.map(code => <CourseCard code={code} />)}
+        </div>
+      </div>
 
-      <p>
-        <span className='font-bold'>Current:</span>{' '}
-        {current && current.join(', ')}
-      </p>
+      <div className='mt-8'>
+        <p className='text-2xl mb-4'>Completed Courses</p>
+
+        <div className='flex flex-row gap-x-4 overflow-x-auto snap-x snap-mandatory'>
+          {completed && completed.map(code => <CourseCard code={code} />)}
+        </div>
+      </div>
+
+      <div className='mt-8'>
+        <p className='text-2xl mb-4'>Currently Taking</p>
+
+        <div className='flex flex-row gap-x-4 overflow-x-auto snap-x snap-mandatory'>
+          {current && current.map(code => <CourseCard code={code} />)}
+        </div>
+      </div>
     </div>
   )
 }
