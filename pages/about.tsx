428452620cfa74cd5fb0b319953afbd27678aec3
Jinho Yoon <yoonjinho0318@gmail.com>
2022-12-08 14:55:20 -0500

    feat: add Q/A on the about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -4,9 +4,14 @@ import Title from '@components/Title'
 const About = () => {
   return (
     <Main>
-      <Title>Cloudberry FAQ</Title>
+      <Title className='mb-8'>Cloudberry FAQ</Title>
 
-      <p>TODO</p>
+      <p className='text-2xl mb-4'>Why is it called Cloudberry?</p>
+
+      <p>
+        Cloudberry is the name of a white squirrel that is often seen on
+        McGill's campus! It is an unofficial mascot of McGill.
+      </p>
     </Main>
   )
 }
