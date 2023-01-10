ee29a124f794e05e62fef24d8c7236639084794c
Jinho Yoon <yoonjinho0318@gmail.com>
2023-01-10 18:12:17 -0500

    feat(style): compact subject labels

--- components/home/filters/SelectedSubjects.tsx
+++ components/home/filters/SelectedSubjects.tsx
@@ -12,12 +12,12 @@ const SelectedSubjects = ({
   return (
     <div className='flex flex-row flex-wrap gap-y-2'>
       {subjects.map((subject, idx) => (
-        <div key={idx} className='mr-2'>
+        <div key={idx} className='mr-2 group'>
           <div className='inline-flex w-full justify-between rounded-full px-4 py-1 border border-transparent bg-violet-100 text-violet-700 focus:outline-none mr-2 flex-wrap'>
             <span className='font-bold'>{subject.code}</span>
-            <span className='hidden md:ml-1 md:inline-block'>
+            {/* <span className='hidden md:ml-1 group-hover:inline-block'>
               {subject.title}
-            </span>
+            </span> */}
             <button
               onClick={() => {
                 localStorage.setItem(
