f2d02cf623419a168c90a308713fe7d4ef11a069
Jinho Yoon <yoonjinho0318@gmail.com>
2023-03-11 11:18:39 -0500

    style(course): move subtitle above semesters

--- pages/courses/[code].tsx
+++ pages/courses/[code].tsx
@@ -35,37 +35,53 @@ const Course = ({
     >
       <div className='flex flex-row w-full'>
         <div className='lg:w-3/5 text-base lg:text-lg'>
-          <div className='space-y-4'>
-            <div className='text-2xl md:text-3xl mt-4 flex flex-row items-center'>
-              <span>
-                <span className='font-semibold mr-2'>{displayCode(code)}</span>
-                <span>{course.name}</span>
-              </span>
-              <ShareButton />
-            </div>
-            <Semesters terms={course.terms} />
-            <div className='text-gray-700 text-base flex flex-row gap-x-2 items-center'>
-              <div>
-                {String(course.credits)}{' '}
-                {course.credits !== 1 ? 'credits' : 'credit'}
-              </div>
-              <div className='w-1 h-1 rounded-full bg-gray-700'></div>
-              <div>{subjectsData[course.code.split('-')[0]]}</div>
-              <div className='w-1 h-1 rounded-full bg-gray-700'></div>
-              <div>Faculty of Science</div>
-            </div>
-            {user && <Actions code={code} />}
-          </div>
-          <div className='mt-10'>
-            <p className='text-2xl font-medium pb-3 mb-4 border-b'>Overview</p>
-            <p>{formatDesc(course.description, code)}</p>
-          </div>
+          <CourseHeading {...{ course, code, user }} />
+
+          <Overview {...{ course, code }} />
           <Notes notes={course.notes} />
           <PrereqsOf prereqsOf={course.prereqsOf} />
+
           <VSBData vsb={course.vsb} />
         </div>
       </div>
     </Main>
+  )
+}
+
+const CourseHeading = ({ course, code, user }) => {
+  return (
+    <div className='space-y-3'>
+      <div className='text-2xl md:text-3xl mt-4 flex flex-row items-center'>
+        <span>
+          <span className='font-semibold mr-2'>{displayCode(code)}</span>
+          <span>{course.name}</span>
+        </span>
+        <ShareButton />
+      </div>
+
+      <div className='text-gray-700 text-base flex flex-row flex-wrap gap-x-2 items-center'>
+        <div>
+          {String(course.credits)} {course.credits !== 1 ? 'credits' : 'credit'}
+        </div>
+        <div className='w-1 h-1 rounded-full bg-gray-700'></div>
+        <div>{subjectsData[course.code.split('-')[0]]}</div>
+        <div className='w-1 h-1 rounded-full bg-gray-700'></div>
+        <div>Faculty of Science</div>
+      </div>
+
+      <Semesters terms={course.terms} />
+
+      {user && <Actions code={code} />}
+    </div>
+  )
+}
+
+const Overview = ({ course, code }) => {
+  return (
+    <div className='mt-10'>
+      <p className='text-2xl font-medium pb-3 mb-4 border-b'>Overview</p>
+      <p>{formatDesc(course.description, code)}</p>
+    </div>
   )
 }
 
