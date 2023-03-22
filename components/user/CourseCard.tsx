faf69e40deb4ac467853594e63589578ee2ce709
lil1an <lilan0213@yahoo.com>
2023-03-21 22:29:53 -0400

    feat(user): make waiter list visible on user page

--- components/user/CourseCard.tsx
+++ components/user/CourseCard.tsx
@@ -1,8 +1,7 @@
-import useCourse from "@hooks/useCourse"
-import Link from "next/link"
+import useCourse from '@hooks/useCourse'
+import Link from 'next/link'
 
-
-const CourseCard = ({ code }: { code: string }) => {
+const CourseCard = ({ code }: { code?: string }) => {
   const format = (code: string) => code?.replace('-', ' ').toUpperCase()
 
   const { data, isLoading } = useCourse(code)
@@ -20,10 +19,13 @@ const CourseCard = ({ code }: { code: string }) => {
   const course = data.result
 
   return (
-    <Link href={`/courses/${code}`} className='flex flex-row gap-x-4 '>
+    <Link
+      href={`/courses/${code}`.toLowerCase()}
+      className='flex flex-row gap-x-4 '
+    >
       <div className='w-60 border rounded-lg p-2 px-4 flex-shrink-0 snap-start'>
-        <p className='font-semibold'>{format(course.code)}</p>
-        <p className=''>{course.name}</p>
+        <p className='font-semibold'>{format(course?.code)}</p>
+        <p className=''>{course?.name}</p>
       </div>
     </Link>
   )
