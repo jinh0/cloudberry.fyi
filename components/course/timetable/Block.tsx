0f618b53492a723ee5de991df8f0bdefb54889cc
Jinho Yoon <yoonjinho0318@gmail.com>
2023-03-27 17:30:17 -0400

    fix(course): set overflow for timetable text

--- components/course/timetable/Block.tsx
+++ components/course/timetable/Block.tsx
@@ -4,19 +4,18 @@ import { displayTime } from '@utils/formatting'
 const Block = ({ block }: { block: VSBTime & { name: string } }) => {
   return (
     <div
-      className='bg-pink-50 px-2 py-1 absolute w-full text-sm'
+      className='bg-pink-50 px-2 py-1 absolute w-full text-sm overflow-auto'
       style={{
         top: `${Math.floor(((block.t1 - 480) / 780) * 100)}%`,
         height: `${((block.t2 - block.t1) / 780) * 100}%`,
       }}
     >
       <div className='text-pink-700 font-bold'>{block.name}</div>
-      <div className='text-pink-600'>
-        {displayTime(block.t1)}{' '}
-        <span className='hidden lg:inline-block'>
-          &minus; {displayTime(block.t2)}
-        </span>
-      </div>
+      {block.t2 - block.t1 > 50 && (
+        <div className='text-pink-600 overflow-auto whitespace-nowrap'>
+          {displayTime(block.t1)} &minus; {displayTime(block.t2)}
+        </div>
+      )}
     </div>
   )
 }
