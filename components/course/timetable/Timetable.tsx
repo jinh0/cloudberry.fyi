98b33a5440da7c1e73cc721d4780a6d1b9c5c3ac
Jinho Yoon <yoonjinho0318@gmail.com>
2023-03-27 11:08:41 -0400

    feat(course): add time-based height

--- components/course/timetable/Timetable.tsx
+++ components/course/timetable/Timetable.tsx
@@ -15,15 +15,19 @@ const NUM_TO_DAY = {
 const Block = ({ block }: { block: VSBTime & { name: string } }) => {
   const { height } = useContext(HeightContext)
 
+  console.log('height', Math.floor(((block.t2 - block.t1) / 720) * 100))
+  console.log(block.t2, block.t1)
+
   return (
     <div
-      className='border rounded-lg relative'
+      className='bg-pink-50 px-2 py-1 absolute w-full text-sm'
       style={{
-        top: `${Math.floor(((block.t2 - block.t1) / height) * 100)}%`,
+        top: `${Math.floor(((block.t1 - 480) / 780) * 100)}%`,
+        height: `${((block.t2 - block.t1) / 780) * 100}%`,
       }}
     >
-      <div>{block.name}</div>
-      <div>
+      <div className='text-pink-700 font-bold'>{block.name}</div>
+      <div className='text-pink-500'>
         {displayTime(block.t1)} - {displayTime(block.t2)}
       </div>
     </div>
@@ -43,7 +47,19 @@ const Day = ({ num, blocks }: { num: number; blocks: VSBBlock[] }) => {
     <div className='w-1/5 h-full flex flex-col'>
       <div className='w-full text-center'>{NUM_TO_DAY[num][0]}</div>
 
-      <div className={`w-full border flex-1`}>
+      <div
+        className={`w-full border-r flex-1 relative
+      `}
+        style={{
+          background: `repeating-linear-gradient(
+            to bottom,
+            #ffffff 0%,
+            #ffffff ${100 / 13 - 0.2}%,
+            #eeeeee ${100 / 13 - 0.2}%,
+            #eeeeee ${100 / 13}%
+          )`,
+        }}
+      >
         {todaysSchedule.map((x, idx) => (
           <Block key={idx} block={x} />
         ))}
@@ -67,7 +83,26 @@ const Timetable = () => {
 
   return (
     <HeightContext.Provider value={{ height }}>
-      <div className={`w-full border flex flex-row text-base h-[400px]`}>
+      <div className={`w-full flex flex-row text-base h-[500px]`}>
+        <div className='flex flex-col pr-1 text-gray-500'>
+          <div className='w-full text-center text-transparent'>12:00</div>
+          <div className='relative h-full top-0'>
+            {[8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20].map(
+              (x, idx) => (
+                <div
+                  key={idx}
+                  style={{
+                    top: `${(idx / 13) * 100}%`,
+                  }}
+                  className='absolute w-fit -mt-3'
+                >
+                  {x}:00
+                </div>
+              )
+            )}
+          </div>
+        </div>
+
         <Day num={1} blocks={chosen} />
         <Day num={2} blocks={chosen} />
         <Day num={3} blocks={chosen} />
