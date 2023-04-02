2a437065db970e7bf96252f6ac6c48cc0aaf0757
Jinho Yoon <yoonjinho0318@gmail.com>
2023-04-02 19:13:19 -0400

    refactor(course): remove usage of utils/lookup.json

--- pages/courses/[code].tsx
+++ pages/courses/[code].tsx
@@ -5,6 +5,7 @@
 import vsbCourses from '@utils/vsb'
 import prereqsOf from 'public/prereqs-of.json'
 import courseData from 'public/full-data.json'
+import lookup from 'public/lookup.json'
 
 import { displayCode } from '@utils/formatting'
 
@@ -19,32 +20,41 @@ import Notes from '@components/course/Notes'
 import PrereqsOf from '@components/course/PrereqsOf'
 import CourseHeading from '@components/course/CourseHeading'
 import Overview from '@components/course/Overview'
+import useLookup from '@hooks/useLookup'
+import LookupContext from '@contexts/LookupContext'
 
 const Course = ({
   course,
 }: {
-  course: CourseType & { vsb: VSBCourse; prereqsOf: string[] }
+  course: CourseType & {
+    vsb: VSBCourse
+    prereqsOf: Array<{ code: Lowercase<string>; title: string }>
+  }
 }) => {
   const router = useRouter()
   const { code } = router.query as { code: string }
   const { user } = useUser()
+
+  const { lookup, isLoading } = useLookup()
 
   return (
     <Main
       title={`${displayCode(code)} ${course.title} | Cloudberry`}
       content={course.description}
     >
-      <div className='flex flex-row w-full'>
-        <div className='lg:w-3/5 text-base lg:text-lg'>
-          <CourseHeading {...{ course, code, user }} />
+      <LookupContext.Provider value={{ lookup, isLoading }}>
+        <div className='flex flex-row w-full'>
+          <div className='lg:w-3/5 text-base lg:text-lg'>
+            <CourseHeading {...{ course, code, user }} />
 
-          <Overview {...{ course, code }} />
-          <Notes notes={course.notes} />
-          <PrereqsOf prereqsOf={course.prereqsOf} />
+            <Overview {...{ course, code }} />
+            <Notes notes={course.notes} />
+            <PrereqsOf prereqsOf={course.prereqsOf} />
 
-          <VSBData vsb={course.vsb} />
+            <VSBData vsb={course.vsb} />
+          </div>
         </div>
-      </div>
+      </LookupContext.Provider>
     </Main>
   )
 }
@@ -69,14 +79,19 @@ export async function getStaticProps({ params }: { params: { code: string } }) {
     course => course.code.toLowerCase() === params.code.toLowerCase()
   )
 
-  const prereqsOfData = prereqsOf[params.code.toLowerCase()]
+  const prereqs: Lowercase<string>[] = prereqsOf[params.code.toLowerCase()]
+
+  const prereqPopulated = prereqs.map(x => ({
+    code: x,
+    title: lookup[x.toUpperCase()],
+  }))
 
   return {
     props: {
       course: {
         ...courseData,
         vsb: vsbData ? vsbData : null,
-        prereqsOf: prereqsOfData ? prereqsOfData : [],
+        prereqsOf: prereqs ? prereqPopulated : [],
       },
     },
   }
