419f330861bdc08ae72f0e154d1a75f739018497
Jinho Yoon <yoonjinho0318@gmail.com>
2023-04-03 09:02:24 -0400

    feat(db): add year/semester to vsb

--- db/migration.ts
+++ db/migration.ts
@@ -3,6 +3,14 @@ import prisma from './client'
 async function migrate() {
   await prisma.$connect()
 
+  await prisma.vsb.updateMany({
+    where: {},
+    data: {
+      year: 2022,
+      semester: 'winter',
+    },
+  })
+
   await prisma.$disconnect()
 }
 
