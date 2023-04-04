cd43a78db74969a4fe413be907ff08a504f9d3b7
Jinho Yoon <yoonjinho0318@gmail.com>
2023-04-03 22:12:55 -0400

    fix: remove build-time gen import

--- db/migration.ts
+++ db/migration.ts
@@ -1,26 +1,7 @@
 import prisma from './client'
-import courses from 'webscraper/data/2022/course-titles.json'
 
 async function migrate() {
   await prisma.$connect()
-
-  console.log(Object.keys(courses).length)
-
-  let x = 0
-
-  Object.keys(courses).forEach(async code => {
-    const course = await prisma.vsb.findFirst({
-      where: {
-        code,
-      },
-    })
-
-    if (course === null) {
-      x++
-      console.log(code)
-    }
-  })
-
   await prisma.$disconnect()
 }
 
