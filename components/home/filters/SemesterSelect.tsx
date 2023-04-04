e4cc3fdfff018544d940994c29bc351da6f72a0c
lil1an <lilan0213@yahoo.com>
2023-04-03 22:09:06 -0400

    feat(home): add colour to filter option

--- components/home/filters/SemesterSelect.tsx
+++ components/home/filters/SemesterSelect.tsx
@@ -5,7 +5,7 @@ import { Listbox } from '@headlessui/react'
 import { faPagelines } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { CheckIcon, ChevronDownIcon } from '@heroicons/react/24/solid'
-import { faSnowflake } from '@fortawesome/free-solid-svg-icons'
+import { faSnowflake, faSun } from '@fortawesome/free-solid-svg-icons'
 import { XCircleIcon } from '@heroicons/react/24/outline'
 import SearchContext from '@contexts/SearchContext'
 
@@ -13,6 +13,7 @@ const semesters = [
   { id: 0, name: 'All Semesters', value: 'fall|winter' },
   { id: 1, name: 'Fall 2022', value: 'fall' },
   { id: 2, name: 'Winter 2023', value: 'winter' },
+  { id: 3, name: 'Summer 2023', value: 'summer' },
 ]
 
 const classNames = (...args: string[]) => args.join(' ')
@@ -32,7 +33,7 @@ const semesterStyles = {
   1: (
     <Listbox.Button
       className={classNames(
-        'border rounded-full px-4 py-1 outline-none placeholder-gray-600 w-fit flex flex-row items-start text-gray-600',
+        'border border-red-50 rounded-full px-4 py-1 outline-none placeholder-gray-600 w-fit flex flex-row items-start text-red-600 bg-red-50',
         'flex flex-row items-center'
       )}
     >
@@ -44,7 +45,7 @@ const semesterStyles = {
   2: (
     <Listbox.Button
       className={classNames(
-        'border rounded-full px-4 py-1 outline-none placeholder-gray-600 w-fit flex flex-row items-start text-gray-600',
+        'border border-sky-50 rounded-full px-4 py-1 outline-none placeholder-gray-600 w-fit flex flex-row items-start text-sky-600 bg-sky-50',
         'flex flex-row items-center'
       )}
     >
@@ -56,15 +57,27 @@ const semesterStyles = {
   3: (
     <Listbox.Button
       className={classNames(
-        'border rounded-full px-4 py-1 outline-none placeholder-gray-600 w-fit flex flex-row items-start text-gray-600',
+        'border border-orange-50 rounded-full px-4 py-1 outline-none placeholder-gray-600 w-fit flex flex-row items-start text-orange-600 bg-orange-50',
         'flex flex-row items-center'
       )}
     >
-      <XCircleIcon className='w-5 h-5 mr-2' />
-      Not Offered
+      <FontAwesomeIcon icon={faSun} className='w-4 h-4 mr-2' />
+      Summer 2023
       <ChevronDownIcon className='w-6 h-6 ml-4' />
     </Listbox.Button>
   ),
+  // 4: (
+  //   <Listbox.Button
+  //     className={classNames(
+  //       'border rounded-full px-4 py-1 outline-none placeholder-gray-600 w-fit flex flex-row items-start text-gray-600',
+  //       'flex flex-row items-center'
+  //     )}
+  //   >
+  //     <XCircleIcon className='w-5 h-5 mr-2' />
+  //     Not Offered
+  //     <ChevronDownIcon className='w-6 h-6 ml-4' />
+  //   </Listbox.Button>
+  // ),
 }
 
 const SemesterSelect = () => {
