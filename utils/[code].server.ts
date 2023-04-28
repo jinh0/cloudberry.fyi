79d91e1590339d79171518b1c1af1542f13487ff
Jinho Yoon <yoonjinho0318@gmail.com>
2023-04-27 20:47:29 -0400

    feat(course): use prisma for prereqs

--- utils/[code].server.ts
+++ utils/[code].server.ts
@@ -1,5 +1,4 @@
 import prisma from '@db/client'
-import { readJSON } from './files'
 
 export function getPaths(year: number) {
   return async () => {
@@ -18,7 +17,6 @@ export async function getStaticProps({
 }: {
   params: { code: string; yearRange?: string }
 }) {
-  console.log(process.cwd(), params)
   const year = params.yearRange ? Number(params.yearRange.split('-')[0]) : 2022
 
   const course = await prisma.course.findUnique({
@@ -37,21 +35,18 @@ export async function getStaticProps({
     },
   })
 
-  const prereqsOf = await readJSON(
-    year === 2022
-      ? 'public/prereqs-of.json'
-      : `public/data/${year}/prereqs-of.json`
-  )
-  const lookup = await readJSON(
-    year === 2022 ? 'public/lookup.json' : `public/data/${year}/lookup.json`
-  )
-
-  const prereqs: Lowercase<string>[] = prereqsOf[params.code.toLowerCase()]
-
-  const prereqPopulated = prereqs.map(x => ({
-    code: x,
-    title: lookup[x.toUpperCase()],
-  }))
+  const prereqs = await prisma.course.findMany({
+    where: {
+      year,
+      prerequisites: {
+        has: params.code.toLowerCase(),
+      },
+    },
+    select: {
+      code: true,
+      title: true,
+    },
+  })
 
   return {
     props: {
@@ -59,7 +54,7 @@ export async function getStaticProps({
       course: {
         ...course,
         vsbs: vsbs.map(x => ({ semester: x.semester, vsb: x })),
-        prereqsOf: prereqs ? prereqPopulated : [],
+        prereqsOf: prereqs,
       },
     },
   }
