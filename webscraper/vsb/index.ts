0dc0d21ee6896ffccb7b0d8c3cf559c428d27b65
Jinho Yoon <yoonjinho0318@gmail.com>
2023-04-27 22:50:15 -0400

    feat(scraper): speed up vsb scraper

--- webscraper/vsb/index.ts
+++ webscraper/vsb/index.ts
@@ -1,14 +1,12 @@
 import prisma from '@db/client'
+import { SemesterType } from '@typing'
 import { getCourse } from '@utils/vsbScraper'
 
 const sleep = (milliseconds: number) => {
   return new Promise(resolve => setTimeout(resolve, milliseconds))
 }
 
-export async function getVSBInfo(
-  year: number,
-  semester: 'fall' | 'winter' | 'summer'
-) {
+export async function getVSBInfo(year: number, semester: SemesterType) {
   const courses = await prisma.course.findMany({
     where: {
       year,
@@ -21,40 +19,51 @@ export async function getVSBInfo(
     select: {
       code: true,
     },
+    orderBy: {
+      code: 'asc',
+    },
   })
 
   // Loop through all courses and get relevant data
-  for (const course of courses) {
-    const vsb = await getCourse(
-      course.code as Uppercase<string>,
-      year,
-      semester
+  for (let i = 0; i < courses.length; i += 100) {
+    await Promise.all(
+      courses
+        .slice(i, i + 100)
+        .map(course => saveCourse(course, year, semester))
     )
 
-    if (vsb.isOk) {
-      console.log(course.code, ' fetched')
-
-      await prisma.vsb.upsert({
-        create: {
-          ...vsb.result,
-          year,
-          semester,
-        },
-        update: {
-          ...vsb.result,
-          year,
-          semester,
-        },
-        where: {
-          code_year_semester: {
-            code: course.code,
-            year,
-            semester,
-          },
-        },
-      })
-    }
-
-    await sleep(10)
+    console.log(`Course ${i}`)
   }
+}
+
+async function saveCourse(
+  course: { code: string },
+  year: number,
+  semester: SemesterType
+) {
+  const vsb = await getCourse(course.code as Uppercase<string>, year, semester)
+
+  if (vsb.isOk) {
+    await prisma.vsb.upsert({
+      create: {
+        ...vsb.result,
+        year,
+        semester,
+      },
+      update: {
+        ...vsb.result,
+        year,
+        semester,
+      },
+      where: {
+        code_year_semester: {
+          code: course.code,
+          year,
+          semester,
+        },
+      },
+    })
+  }
+
+  await sleep(10)
 }
