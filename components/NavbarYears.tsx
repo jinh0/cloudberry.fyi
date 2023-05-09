3f58d03ad6233822f07b57a7e5925dd085de8279
Jinho Yoon <yoonjinho0318@gmail.com>
2023-05-09 18:47:39 -0400

    feat: add 2023 to navbar years

--- components/NavbarYears.tsx
+++ components/NavbarYears.tsx
@@ -1,10 +1,9 @@
+import { COURSE_YEARS } from '@utils/constants'
 import { courseLink } from '@utils/links'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 
 export default function NavbarYears({ currentYear }: { currentYear: number }) {
-  const years = [2022, 2021, 2020, 2019, 2018]
-
   const { pathname, query } = useRouter()
 
   if (
@@ -14,25 +13,27 @@ export default function NavbarYears({ currentYear }: { currentYear: number }) {
   )
     return (
       <div className='gap-x-4 flex flex-row overflow-scroll py-1 border-b bg-white bg-opacity-60 backdrop-blur-md w-full px-6 lg:px-20'>
-        {years.map(year =>
-          year === currentYear ? (
-            <div key={year} className='font-semibold text-slate-800'>
-              <Link
-                href={courseLink(year, (query.code as string).toLowerCase())}
-              >
-                {year}-{year + 1}
-              </Link>
-            </div>
-          ) : (
-            <div key={year} className='text-slate-600'>
-              <Link
-                href={courseLink(year, (query.code as string).toLowerCase())}
-              >
-                {year}-{year + 1}
-              </Link>
-            </div>
-          )
-        )}
+        {COURSE_YEARS.slice()
+          .reverse()
+          .map(year =>
+            year === currentYear ? (
+              <div key={year} className='font-semibold text-slate-800'>
+                <Link
+                  href={courseLink(year, (query.code as string).toLowerCase())}
+                >
+                  {year}-{year + 1}
+                </Link>
+              </div>
+            ) : (
+              <div key={year} className='text-slate-600'>
+                <Link
+                  href={courseLink(year, (query.code as string).toLowerCase())}
+                >
+                  {year}-{year + 1}
+                </Link>
+              </div>
+            )
+          )}
       </div>
     )
 
