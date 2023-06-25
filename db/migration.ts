6eac114447b80cf04b9fb619c4716b25e227ea2c
Jinho Yoon <yoonjinho0318@gmail.com>
2023-06-25 19:34:54 -0400

    feat: add capacity to classes

--- db/migration.ts
+++ db/migration.ts
@@ -2,6 +2,24 @@ import prisma from './client'
 
 async function migrate() {
   await prisma.$connect()
+
+  const vsbs = await prisma.vsb.findMany({
+    where: {
+      year: 2023
+    }
+  });
+
+  for (const x of vsbs) {
+    console.log(x.code);
+
+    await prisma.vsb.update({
+      where: { id: x.id },
+      data: {
+        blocks: x.blocks.map(block => ({...block, capacity: block.remainingSeats }))
+      }
+    })
+  }
+
   await prisma.$disconnect()
 }
 
